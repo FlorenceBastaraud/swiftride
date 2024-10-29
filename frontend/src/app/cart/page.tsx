854b75ac86c0cee8d12db9d96744a391659a9382
Florence Bastaraud <fbastaraud@yahoo.fr>
2024-10-29 23:32:46 +0100

    added no items in cart text in cart page

--- frontend/src/app/cart/page.tsx
+++ frontend/src/app/cart/page.tsx
@@ -17,35 +17,47 @@ export default function Cart() {
       <h1 className="relative flex items-center justify-center z-10 text-4xl md:text-5xl font-bold text-black text-center px-4">
         Cart <span className="text-sm ml-2">({cartTotal})</span>
       </h1>
-      <div className="flex flex-col mt-8 space-y-6 px-4 md:px-8">
-        <div className="flex flex-col space-y-4">
-          {cart.length > 0 &&
-            cart.map((item) => (
-              <CartProductCard key={item.product.Slug} item={item} />
-            ))}
-        </div>
-
-        <div className="p-4 border-t border-gray-200 mt-6">
-          <div className="flex justify-between text-lg font-semibold text-gray-800">
-            <span>Total:</span>
-            <span>${cartTotalPrice.toFixed(2)}</span>
+      {cartTotal > 0 ? (
+        <div className="flex flex-col mt-8 space-y-6 px-4 md:px-8">
+          <div className="flex flex-col space-y-4">
+            {cart.length > 0 &&
+              cart.map((item) => (
+                <CartProductCard key={item.product.Slug} item={item} />
+              ))}
           </div>
-          {cartTotal > 0 && (
-            <button
-              className="mt-4 w-full py-2 text-black bg-slate-200 rounded-md hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300"
-              onClick={deleteCart}
+
+          <div className="p-4 border-t border-gray-200 mt-6">
+            <div className="flex justify-between text-lg font-semibold text-gray-800">
+              <span>Total:</span>
+              <span>${cartTotalPrice.toFixed(2)}</span>
+            </div>
+            {cartTotal > 0 && (
+              <button
+                className="mt-4 w-full py-2 text-black bg-slate-200 rounded-md hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300"
+                onClick={deleteCart}
+              >
+                Clear
+              </button>
+            )}
+            <Link
+              href="/checkout"
+              className="mt-4 block text-center w-full py-2 text-white bg-black rounded-md hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300"
             >
-              Clear
-            </button>
-          )}
+              Proceed to Checkout
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center mt-10 gap-3">
+          <p className="text-lg">There are no items in your cart.</p>
           <Link
-            href="/checkout"
-            className="mt-4 block text-center w-full py-2 text-white bg-black rounded-md hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300"
+            href="/shop"
+            className="text-white bg-black py-1 px-4 border border-transparent rounded-2xl transition-effect hover:bg-white hover:text-black hover:border-black"
           >
-            Proceed to Checkout
+            Shop
           </Link>
         </div>
-      </div>
+      )}
     </main>
   )
 }

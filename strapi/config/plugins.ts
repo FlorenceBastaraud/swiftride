9e7b5e2686895c1726c51799f13314cf70e96454
Florence Bastaraud <fbastaraud@yahoo.fr>
2024-12-19 10:46:28 +0100

    aws config plugins strapi

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,15 +1,12 @@
 export default () => ({
   upload: {
-    config: {
-      provider: 'aws-s3',
-      providerOptions: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: process.env.AWS_REGION,
-        params: {
-          Bucket: process.env.AWS_BUCKET_NAME,
-        },
-        acl: null,
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: process.env.AWS_REGION,
+      params: {
+        Bucket: process.env.AWS_BUCKET_NAME,
       },
     },
   },

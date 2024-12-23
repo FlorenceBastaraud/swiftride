9b9030418e212ed1fc305e5eeeb59881ece30a13
Florence Bastaraud <fbastaraud@yahoo.fr>
2024-12-23 10:40:54 +0100

    Revert "changed cors"
    This reverts commit 708908445d5bfe9b49b1871510c9f140fbf3f40b.
    
    back to pre prod config state

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -2,18 +2,11 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['http://localhost:3000', 'https://swiftride.onrender.com'],
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization'],
-    },
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-]
+];

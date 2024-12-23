d49f316203c6114a4a01be1f3f004ca70be4d0af
Florence Bastaraud <fbastaraud@yahoo.fr>
2024-12-23 10:40:03 +0100

    Revert "aws config plugins strapi"
    This reverts commit 9e7b5e2686895c1726c51799f13314cf70e96454.
    
    back to pre prod config state

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,12 +1,15 @@
 export default () => ({
   upload: {
-    provider: 'aws-s3',
-    providerOptions: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      region: process.env.AWS_REGION,
-      params: {
-        Bucket: process.env.AWS_BUCKET_NAME,
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION,
+        params: {
+          Bucket: process.env.AWS_BUCKET_NAME,
+        },
+        acl: null,
       },
     },
   },

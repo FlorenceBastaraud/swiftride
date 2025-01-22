a26b8ead7a97d560926119c58da98bd961134174
Florence Bastaraud <fbastaraud@yahoo.fr>
2025-01-22 14:42:03 +0100

    db config

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -2,14 +2,8 @@ export default ({ env }) => {
   return {
     connection: {
       client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        user: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', false),
-      },
+      connection: env('DATABASE_URL'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
   }
 }
